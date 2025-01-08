3c7c66c8aace1e4cd2972788f22b5a3645d87148
Shahar Margalit <shaharma@gmail.com>
2025-01-08 10:42:16 +0200

    Add service for handling memes

--- js/canvas.util.js
+++ js/canvas.util.js
@@ -15,3 +15,18 @@ function drawText(text, x, y) {
   gCtx.fillText(text, x, y)
   // gCtx.strokeText(text, x, y)
 }
+
+function loadImageFromSrc(imgData, onImageReady) {
+  // document.querySelector('.share-container').innerHTML = ''
+  const img = new Image()
+  img.onload = () => {
+    onImageReady(img)
+  }
+  img.src = imgData
+}
+
+function renderImg(img) {
+  gElCanvas.height = (img.naturalHeight / img.naturalWidth) * gElCanvas.width
+  gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+  drawText('Hola!', gElCanvas.width / 2, 20) // 20px half font size
+}
--- js/editor.controller.js
+++ js/editor.controller.js
@@ -14,11 +14,12 @@ function onInit() {
 }
 
 function renderMeme() {
-  const elImg = new Image()
-  elImg.src = 'img/1.jpg'
-  elImg.onload = () => {
-    coverCanvasWithImg(elImg)
-    drawText('Hola!', gElCanvas.width / 2, 20) // 20px half font size
-
-  }
+  // const elImg = new Image()
+  // elImg.src = 'img/1.jpg'
+  // elImg.onload = () => {
+  //   coverCanvasWithImg(elImg)
+  //   drawText('Hola!', gElCanvas.width / 2, 20) // 20px half font size
+  // }
+  let { data } = getMemeById('')
+  loadImageFromSrc(data, renderImg)
 }
