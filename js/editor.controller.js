714738006ed9c37f2727f9ad0800ffe4776b17ec
Shahar Margalit <shaharma@gmail.com>
2025-01-08 10:52:41 +0200

    Feat edit text

--- js/editor.controller.js
+++ js/editor.controller.js
@@ -14,12 +14,17 @@ function onInit() {
 }
 
 function renderMeme() {
-  // const elImg = new Image()
-  // elImg.src = 'img/1.jpg'
-  // elImg.onload = () => {
-  //   coverCanvasWithImg(elImg)
-  //   drawText('Hola!', gElCanvas.width / 2, 20) // 20px half font size
-  // }
-  let { data } = getMemeById('')
-  loadImageFromSrc(data, renderImg)
+  let meme = getMemeById('')
+  const elImg = new Image()
+  elImg.src = meme.src
+  elImg.onload = () => {
+    coverCanvasWithImg(elImg)
+    drawText(meme.line1, gElCanvas.width / 2, 20) // 20px half font size
+  }
+}
+
+function onAddTxt(txt) {
+  console.log(txt)
+  setLineTxt(txt)
+  renderMeme()
 }
